1788288c4c1275245f8eb2fe5585a55fc5686bc7
Abdi Fatih <69069374+expressionless20@users.noreply.github.com>
2021-06-09 12:36:10 +0700

    Update formProduk.py

--- formProduk.py
+++ formProduk.py
@@ -1,10 +1,3 @@
-# file: formSupplier.py
-# desain form untuk data supplier
-# tgl_buat: 5 okt 2010 01.48 PM
-# tgl_revisi : -
-# lisensi: GPL
-# dibuat oleh: masbiggie@PythonDahsyat.blogspot.com
-
 from tkinter import *
 import sqlite3
 import tkinter.messagebox as tkMessageBox
